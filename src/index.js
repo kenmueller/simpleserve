717d21a5ecbd92333cf9be2cbecd7f7bb1988e54
kenmueller <kenmueller0@gmail.com>
2020-07-09 19:52:36 -0700

    removed middleware dependency

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const { readFile: _readFile } = require('fs')
 const { join } = require('path')
 const express = require('express')
 const { compile } = require('handlebars')
-const { series } = require('middleware-flow')
 
 const cwd = process.cwd()
 
@@ -20,66 +19,62 @@ module.exports = ({
 	optimize = process.env.NODE_ENV === 'production',
 	pages = 'pages',
 	public = 'public'
-} = {}) => {
-	const middlewares = []
-	
-	if (public)
-		middlewares.push(express.static(join(cwd, public)))
-	
-	if (pages)
-		middlewares.push(async (req, res, next) => {
-			try {
-				const { method, url } = req
-				
-				if (method !== 'GET')
-					return next()
-				
-				let cachedRequestValues = requestCache[url]
-				let { path, status, getProps } = cachedRequestValues || {}
-				
-				if (!(optimize && cachedRequestValues)) {
-					try {
-						const path = join(cwd, 'pages', url)
-						
-						cachedRequestValues = {
-							status: 200,
-							path,
-							getProps: require(path)
-						}
-					} catch {
-						const path = join(cwd, 'pages', '404')
-						
-						cachedRequestValues = {
-							status: 404,
-							path,
-							getProps: require(path)
-						}
-					}
+} = {}) => (req, res, next) => {
+	const fallback = async () => {
+		try {
+			const { method, url } = req
+			
+			if (method !== 'GET')
+				return next()
+			
+			let cachedRequestValues = requestCache[url]
+			let { path, status, getProps } = cachedRequestValues || {}
+			
+			if (!(optimize && cachedRequestValues)) {
+				try {
+					const path = join(cwd, 'pages', url)
 					
-					;({ path, status, getProps } = (
-						requestCache[url] = cachedRequestValues
-					))
+					cachedRequestValues = {
+						status: 200,
+						path,
+						getProps: require(path)
+					}
+				} catch {
+					const path = join(cwd, 'pages', '404')
+					
+					cachedRequestValues = {
+						status: 404,
+						path,
+						getProps: require(path)
+					}
 				}
 				
-				const props = typeof getProps === 'function'
-					? await getProps(req)
-					: getProps
-				
-				res.status(status)
-				
-				const cachedTemplate = templateCache[path]
-				
-				if (optimize && cachedTemplate)
-					return res.send(cachedTemplate(props))
-				
-				const data = await readFile(`${path}.html`)
-					.catch(() => readFile(join(path, 'index.html')))
-				
-				res.send((templateCache[path] = compile(data.toString()))(props))
-			} catch (error) {
-				res.status(500).json(error)
+				;({ path, status, getProps } = (
+					requestCache[url] = cachedRequestValues
+				))
 			}
-		})
+			
+			const props = typeof getProps === 'function'
+				? await getProps(req)
+				: getProps
+			
+			res.status(status)
+			
+			const cachedTemplate = templateCache[path]
+			
+			if (optimize && cachedTemplate)
+				return res.send(cachedTemplate(props))
+			
+			const data = await readFile(`${path}.html`)
+				.catch(() => readFile(join(path, 'index.html')))
+			
+			res.send((templateCache[path] = compile(data.toString()))(props))
+		} catch (error) {
+			res.status(500).json(error)
+		}
+	}
 	
-	return series(...middlewares)
+	public
+		? express.static(join(cwd, public))(req, res, fallback)
+		: fallback()
 }
